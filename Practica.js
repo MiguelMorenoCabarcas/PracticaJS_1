cfe620a92d05fb7ce26b5603abc3b9b28c4e8ce2
MiguelMorenoCabarcas <mamoreno2003@gmail.com>
2022-01-25 21:51:53 -0500

    Practicas JS

---
+++ Practica.js
@@ -0,0 +1,44 @@
+ //EJERCICIO 1 NUMERO POSITIVO NEGATIVO 
+
+/*  let num = 3
+
+
+if (num==0) {
+    console.log(`el numero es 0`)    
+}
+if (num > 0) {
+    console.log(`el numero es positivo`)    
+} else if (num < 0){
+    console.log(`el numero es negativo`)   
+}  */
+ 
+//EJERCICIO 2 CONDICIONES
+
+/*  let num = 0
+
+if (num > 5) {
+    console.log(`el numero es mayor que cinco`)
+}
+if (num==0) {
+    console.log(`el numero es 0`)
+}
+if (num < 5 && num > 3) {
+    console.log(`el numero es menor que 5 y mayor que 3`)
+}
+if (num > 10 && num < 60) {
+    console.log(`el numero es mayor que 10 y menor que 60`)
+}
+if (num < 3 && num != 0) {
+    console.log(`el numero es mayor que 3 y no es 0`)
+}
+if (num < 0) {
+    console.log(`el numero es menor que 0`)
+}  */
+
+/* EJERCICIO 3 ULTIMO DIGITO DE UN NUMERO */
+
+/* let num = 45
+  
+let ultimo_digito = num % 10
+    
+console.log(`el ultimo digito de ${num} es ${ultimo_digito}`) */
